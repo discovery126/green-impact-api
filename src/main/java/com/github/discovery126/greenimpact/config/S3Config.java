6ccd97078f850919dfac8374720f7ab7e33fc8a6
discovery126 <den4ikus03@mail.ru>
2025-03-31 23:33:08 +0300

    Integrating VK Cloud and creating a configuration for it

---
+++ src/main/java/com/github/discovery126/greenimpact/config/S3Config.java
@@ -0,0 +1,38 @@
+package com.github.discovery126.greenimpact.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+public class S3Config {
+
+    @Value("${s3.access-key}")
+    private String accessKey;
+
+    @Value("${s3.secret-key}")
+    private String secretKey;
+
+    @Getter
+    @Value("${s3.bucket-name}")
+    private String bucketName;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .endpointOverride(URI.create("https://hb.ru-msk.vkcloud-storage.ru")) // VK Cloud API
+                .region(Region.of("ru-msk")) // Регион
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)
+                ))
+                .build();
+    }
+
+}
