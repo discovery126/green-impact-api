bd1325ea2a684a37127d4934d7c169bbf2fe59cf
discovery126 <den4ikus03@mail.ru>
2025-04-04 17:22:18 +0300

    Fixed JwtRequestFilter, added access denial handler

--- src/main/java/com/github/discovery126/greenimpact/config/SecurityConfig.java
+++ src/main/java/com/github/discovery126/greenimpact/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.github.discovery126.greenimpact.config;
 
+import com.github.discovery126.greenimpact.exception.CustomAccessDeniedHandler;
 import com.github.discovery126.greenimpact.repository.UserRepository;
 import com.github.discovery126.greenimpact.security.JwtRequestFilter;
 import com.github.discovery126.greenimpact.service.CustomDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -19,6 +21,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
@@ -55,7 +58,9 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtRequestFilter jwtRequestFilter)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   JwtRequestFilter jwtRequestFilter,
+                                                   CustomAccessDeniedHandler customAccessDeniedHandler)
             throws Exception {
        http
                .cors(corsConfigurer -> corsConfigurer
@@ -76,7 +81,11 @@ public class SecurityConfig {
                                        "/v1/login",
                                        "/error").permitAll()
                                .anyRequest().authenticated())
-               .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+               .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+               .exceptionHandling(e -> {
+                   e.accessDeniedHandler(customAccessDeniedHandler);
+                   e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+               });
 
        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
        return http.build();
--- src/main/java/com/github/discovery126/greenimpact/security/JwtRequestFilter.java
+++ src/main/java/com/github/discovery126/greenimpact/security/JwtRequestFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,7 +17,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-@Slf4j
 public class JwtRequestFilter extends OncePerRequestFilter {
     private final JwtTokenUtils jwtTokenUtils;
     private final UserDetailsService userDetailsService;
@@ -33,15 +31,17 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authHeader =request.getHeader("Authorization");
         String email = null;
-        String jwt = null;
+        String jwt;
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             jwt = authHeader.substring(7);
             try {
                 email = jwtTokenUtils.extractEmail(jwt);
             } catch (ExpiredJwtException e) {
-                log.info("Время жизни токена вышло");
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Время действие закончилось");
+                return;
             } catch (SignatureException e) {
-                log.info("Подпись неправильная");
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Неправильная JWT подпись");
+                return;
             }
         }
         if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
