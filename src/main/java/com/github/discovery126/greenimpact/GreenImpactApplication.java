24c87c7d2d8f3eba3295817f336354e56f06d2e0
discovery126 <den4ikus03@mail.ru>
2025-04-11 16:14:27 +0300

    Added dotenv and getting environment variables with dotenv

--- src/main/java/com/github/discovery126/greenimpact/GreenImpactApplication.java
+++ src/main/java/com/github/discovery126/greenimpact/GreenImpactApplication.java
@@ -1,14 +1,21 @@
 package com.github.discovery126.greenimpact;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Objects;
 
 @SpringBootApplication
 @EnableScheduling
 public class GreenImpactApplication {
 
 	public static void main(String[] args) {
+		Dotenv.configure()
+				.systemProperties()
+				.load();
 		SpringApplication.run(GreenImpactApplication.class, args);
 	}
 
